1c300d9063e0e25f0232126d05b2528dbda729b0
adama-wzr <adama@ku.edu>
2025-03-30 16:52:04 -0400

    Update main.cu
     - omp_set_num_threads doesn't work with gcc compiler for some reason, so might as well remove it.
    - On Linux I can verify the number of cores running is correct.

--- src/main.cu
+++ src/main.cu
@@ -63,12 +63,6 @@ int main(int argc, char **argv)
 
     // Pore and Particle size distributions
 
-    omp_set_num_threads(opts.nThreads);
-
-    int numThreads = omp_get_num_threads();
-
-    printf("There are %d threads, requested %d\n", numThreads, opts.nThreads);
-
     if(opts.partSD)
         partSD_3D(&opts, &info, &save, P, 1);
     

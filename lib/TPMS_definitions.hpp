1d85f1c02867069bf8f8d20c188bade4dc7da35b
adama-wzr <adama@ku.edu>
2025-04-03 15:55:55 -0400

    Update TPMS_definitions.hpp
    - updated definitions to the new standard.

--- lib/TPMS_definitions.hpp
+++ lib/TPMS_definitions.hpp
@@ -34,7 +34,54 @@ int Gyroid(char *P, float b, meshInfo *info)
             - pointer to meshInfo struct containing mesh parameters
         Outputs:
             - None
+
+        Function will populate the P array based on the iso-bounds, b. In short,
+        the Gyroid structure is defined by the function
+
+        f(x,y,z) = cos(x)sin(y) + cos(y)sin(z) + cos(z)sin(x)
+
+        It can be given a real thickness by defining the iso-values for which the structure
+        is defined, i.e.
+
+        P[x,y,z] = 1 if -b < f(x,y,z) < b
+        P[x,y,z] = 0 otherwise
+
+        This function will populate P according to the criteria above.
+        P is the used as basis for the PSD analysis.
     */
+    int numCellsX = info->numCellsX;
+
+    // Define dx
+
+    float dx = (float)2.0 * PI / (float)numCellsX;
+
+    // we will also calculate SVF based on this discretization
+
+    size_t count = 0;
+    
+    // Begin main loop
+
+    for (long int i = 0; i < info->nElements; i++)
+    {
+        // Break down i into the integer indexes
+        int slice = i / (numCellsX * numCellsX);
+        int row = (i - slice * numCellsX * numCellsX) / numCellsX;
+        int col = i - slice * numCellsX * numCellsX - row * numCellsX;
+        // Now get x,y,z numbers
+        float x = -PI + dx / 2.0 + (float)col * dx;
+        float y = -PI + dx / 2.0 + (float)row * dx;
+        float z = -PI + dx / 2.0 + (float)slice * dx;
+
+        float f = cos(x) * sin(y) + cos(y) * sin(z) + cos(z) * sin(x);
+
+        if (f > -b && f < b)
+        {
+            P[i] = 1;
+            count++;
+        }
+    }
+
+    info->VF = (float)count / (float)info->nElements;
 
     return 0;
 }
@@ -51,7 +98,56 @@ int TPMS_D(char *P, float b, meshInfo *info)
             - pointer to meshInfo struct containing mesh parameters
         Outputs:
             - None
+        
+        Function will populate the P array based on the iso-bounds, b. In short,
+        the D structure is defined by the function
+
+        f(x,y,z) = sin(x)sin(y)sin(z) + sin(x)cos(y)cos(z) + 
+                   cos(x)sin(y)cos(z) + cos(x)cos(y)sin(z)
+
+        It can be given a real thickness by defining the iso-values for which the structure
+        is defined, i.e.
+
+        P[x,y,z] = 1 if -b < f(x,y,z) < b
+        P[x,y,z] = 0 otherwise
+
+        This function will populate P according to the criteria above.
+        P is the used as basis for the PSD analysis.
     */
+    int numCellsX = info->numCellsX;
+
+    // Define dx
+
+    float dx = (float)2.0 * PI / (float)numCellsX;
+
+    // we will also calculate SVF based on this discretization
+
+    size_t count = 0;
+    
+    // Begin main loop
+
+    for (long int i = 0; i < info->nElements; i++)
+    {
+        // Break down i into the integer indexes
+        int slice = i / (numCellsX * numCellsX);
+        int row = (i - slice * numCellsX * numCellsX) / numCellsX;
+        int col = i - slice * numCellsX * numCellsX - row * numCellsX;
+        // Now get x,y,z numbers
+        float x = -PI + dx / 2.0 + (float)col * dx;
+        float y = -PI + dx / 2.0 + (float)row * dx;
+        float z = -PI + dx / 2.0 + (float)slice * dx;
+
+        float f = sin(x) * sin(y) * sin(z) + sin(x) * cos(y) * cos(z) + 
+                  cos(x) * sin(y) * cos(z) + cos(x) * cos(y) * sin(z);
+
+        if (f > -b && f < b)
+        {
+            P[i] = 1;
+            count++;
+        }
+    }
+
+    info->VF = (float)count / (float)info->nElements;
 
     return 0;
 }
@@ -132,7 +228,55 @@ int IWP(char *P, float b, meshInfo *info)
             - pointer to meshInfo struct containing mesh parameters
         Outputs:
             - None
+        
+        Function will populate the P array based on the iso-bounds, b. In short,
+        the IWP structure is defined by the function
+
+        f(x,y,z) = 2(cos(x)cos(y) + cos(y)cos(z) + cos(z)cos(x)) - (cos(2x) + cos(2y) + cos(2z))
+
+        It can be given a real thickness by defining the iso-values for which the structure
+        is defined, i.e.
+
+        P[x,y,z] = 1 if -b < f(x,y,z) < b
+        P[x,y,z] = 0 otherwise
+
+        This function will populate P according to the criteria above.
+        P is the used as basis for the PSD analysis.
     */
+    int numCellsX = info->numCellsX;
+
+    // Define dx
+
+    float dx = (float)2.0 * PI / (float)numCellsX;
+
+    // we will also calculate SVF based on this discretization
+
+    size_t count = 0;
+
+    // Begin main loop
+
+    for (long int i = 0; i < info->nElements; i++)
+    {
+        // Break down i into the integer indexes
+        int slice = i / (numCellsX * numCellsX);
+        int row = (i - slice * numCellsX * numCellsX) / numCellsX;
+        int col = i - slice * numCellsX * numCellsX - row * numCellsX;
+        // Now get x,y,z numbers
+        float x = -PI + dx / 2.0 + (float)col * dx;
+        float y = -PI + dx / 2.0 + (float)row * dx;
+        float z = -PI + dx / 2.0 + (float)slice * dx;
+
+        float f = 2 * (cos(x) * cos(y) + cos(y) * cos(z) + cos(z) * cos(x)) -
+                  (cos(2 * x) + cos(2 * y) + cos(2 * z));
+
+        if (f > -b && f < b)
+        {
+            P[i] = 1;
+            count++;
+        }
+    }
+
+    info->VF = (float)count / (float)info->nElements;
 
     return 0;
 }
@@ -149,7 +293,53 @@ int Neovius(char *P, float b, meshInfo *info)
             - pointer to meshInfo struct containing mesh parameters
         Outputs:
             - None
+        Function will populate the P array based on the iso-bounds, b. In short,
+        the Neovius structure is defined by the function
+
+        f(x,y,z) = 3(cos(x) + cos(y) + cos(z)) + 4cos(x)cos(y)cos(z)
+
+        It can be given a real thickness by defining the iso-values for which the structure
+        is defined, i.e.
+
+        P[x,y,z] = 1 if -b < f(x,y,z) < b
+        P[x,y,z] = 0 otherwise
+
+        This function will populate P according to the criteria above.
+        P is the used as basis for the PSD analysis.
     */
+    int numCellsX = info->numCellsX;
+
+    // Define dx
+
+    float dx = (float)2.0 * PI / (float)numCellsX;
+
+    // we will also calculate SVF based on this discretization
+
+    size_t count = 0;
+
+    // Begin main loop
+
+    for (long int i = 0; i < info->nElements; i++)
+    {
+        // Break down i into the integer indexes
+        int slice = i / (numCellsX * numCellsX);
+        int row = (i - slice * numCellsX * numCellsX) / numCellsX;
+        int col = i - slice * numCellsX * numCellsX - row * numCellsX;
+        // Now get x,y,z numbers
+        float x = -PI + dx / 2.0 + (float)col * dx;
+        float y = -PI + dx / 2.0 + (float)row * dx;
+        float z = -PI + dx / 2.0 + (float)slice * dx;
+
+        float f = 3 * (cos(x) + cos(y) + cos(z)) + 4 * cos(x) * cos(y) * cos(z);
+
+        if (f > -b && f < b)
+        {
+            P[i] = 1;
+            count++;
+        }
+    }
+
+    info->VF = (float)count / (float)info->nElements;
 
     return 0;
 }
@@ -216,6 +406,204 @@ int C_Y(char *P, float b, meshInfo *info)
     return 0;
 }
 
+// Index 7
+
+int Lidinoid(char *P, float b, meshInfo *info)
+{
+    /*
+        Function Lidinoid:
+        Inputs:
+            - pointer to P, will store binary phase information
+            - float b gives the iso-bounds for solid structure definition
+            - pointer to meshInfo struct containing mesh parameters
+        Outputs:
+            - None
+
+        Function will populate the P array based on the iso-bounds, b. In short,
+        the Lidinoid structure is defined by the function
+
+        f(x,y,z) = (sin(2x)cos(y)sin(z) + sin(x)sin(2y)cos(z) + cos(x)sin(y)sin(2z)) -
+                  (cos(2x)cos(2y) + cos(2y)cos(2z) + cos(2z)cos(2x)) + 0.3
+
+        It can be given a real thickness by defining the iso-values for which the structure
+        is defined, i.e.
+
+        P[x,y,z] = 1 if -b < f(x,y,z) < b
+        P[x,y,z] = 0 otherwise
+
+        This function will populate P according to the criteria above.
+        P is the used as basis for the PSD analysis.
+    */
+    int numCellsX = info->numCellsX;
+
+    // define step dx
+
+    float dx = (float)2.0 * PI / (float)numCellsX;
+
+    // We will also calculate the porosity based on this discretization
+
+    size_t count = 0;
+
+    // Begin main loop
+
+    for (long int i = 0; i < info->nElements; i++)
+    {
+        // Break down i into the integer indexes
+        int slice = i / (numCellsX * numCellsX);
+        int row = (i - slice * numCellsX * numCellsX) / numCellsX;
+        int col = i - slice * numCellsX * numCellsX - row * numCellsX;
+        // Now get x,y,z numbers
+        float x = -PI + dx / 2.0 + (float)col * dx;
+        float y = -PI + dx / 2.0 + (float)row * dx;
+        float z = -PI + dx / 2.0 + (float)slice * dx;
+
+        float f = (sin(2 * x) * cos(y) * sin(z) + sin(x) * sin(2 * y) * cos(z) + cos(x) * sin(y) * sin(2 * z)) -
+                  (cos(2 * x)*cos(2 * y) + cos(2 * y) * cos(2 * z) + cos(2 * z) * cos(2 * x)) + 0.3;
+
+        if (f > -b && f < b)
+        {
+            P[i] = 1;
+            count++;
+        }
+    }
+
+    info->VF = (float)count / (float)info->nElements;
+
+    return 0;
+}
+
+// Index 8
+
+int OCTO(char *P, float b, meshInfo *info)
+{
+    /*
+        Function OCTO:
+        Inputs:
+            - pointer to P, will store binary phase information
+            - float b gives the iso-bounds for solid structure definition
+            - pointer to meshInfo struct containing mesh parameters
+        Outputs:
+            - None
+
+        Function will populate the P array based on the iso-bounds, b. In short,
+        the OCTO structure is defined by the function
+
+        f(x,y,z) = 0.6(cos(x)cos(y) + cos(y)cos(z) + cos(z)cos(x)) -
+                   0.4(cos(x) + cos(y) + cos(z)) + 0.25
+
+        It can be given a real thickness by defining the iso-values for which the structure
+        is defined, i.e.
+
+        P[x,y,z] = 1 if -b < f(x,y,z) < b
+        P[x,y,z] = 0 otherwise
+
+        This function will populate P according to the criteria above.
+        P is the used as basis for the PSD analysis.
+    */
+    int numCellsX = info->numCellsX;
+
+    // define step dx
+
+    float dx = (float)2.0 * PI / (float)numCellsX;
+
+    // We will also calculate the porosity based on this discretization
+
+    size_t count = 0;
+
+    // Begin main loop
+
+    for (long int i = 0; i < info->nElements; i++)
+    {
+        // Break down i into the integer indexes
+        int slice = i / (numCellsX * numCellsX);
+        int row = (i - slice * numCellsX * numCellsX) / numCellsX;
+        int col = i - slice * numCellsX * numCellsX - row * numCellsX;
+        // Now get x,y,z numbers
+        float x = -PI + dx / 2.0 + (float)col * dx;
+        float y = -PI + dx / 2.0 + (float)row * dx;
+        float z = -PI + dx / 2.0 + (float)slice * dx;
+
+        float f = 0.6 * (cos(x) * cos(y) + cos(y) * cos(z) + cos(z) * cos(x)) -
+                  0.4 * (cos(x) + cos(y) + cos(z)) + 0.25;
+
+        if (f > -b && f < b)
+        {
+            P[i] = 1;
+            count++;
+        }
+    }
+
+    info->VF = (float)count / (float)info->nElements;
+
+    return 0;
+}
+
+// Index 9
+
+int FRD(char *P, float b, meshInfo *info)
+{
+    /*
+        Function FRD:
+        Inputs:
+            - pointer to P, will store binary phase information
+            - float b gives the iso-bounds for solid structure definition
+            - pointer to meshInfo struct containing mesh parameters
+        Outputs:
+            - None
+
+        Function will populate the P array based on the iso-bounds, b. In short,
+        the FRD structure is defined by the function
+
+        f(x,y,z) = 8cos(x)cos(y)cos(z) + cos(2x)cos(2y)cos(2z) - 
+                  (cos(2x)cos(2y) + cos(2y)cos(2z) + cos(2z)cos(2x))
+
+        It can be given a real thickness by defining the iso-values for which the structure
+        is defined, i.e.
+
+        P[x,y,z] = 1 if -b < f(x,y,z) < b
+        P[x,y,z] = 0 otherwise
+
+        This function will populate P according to the criteria above.
+        P is the used as basis for the PSD analysis.
+    */
+    int numCellsX = info->numCellsX;
+
+    // define step dx
+
+    float dx = (float)2.0 * PI / (float)numCellsX;
+
+    // We will also calculate the porosity based on this discretization
+
+    size_t count = 0;
+
+    // Begin main loop
+
+    for (long int i = 0; i < info->nElements; i++)
+    {
+        // Break down i into the integer indexes
+        int slice = i / (numCellsX * numCellsX);
+        int row = (i - slice * numCellsX * numCellsX) / numCellsX;
+        int col = i - slice * numCellsX * numCellsX - row * numCellsX;
+        // Now get x,y,z numbers
+        float x = -PI + dx / 2.0 + (float)col * dx;
+        float y = -PI + dx / 2.0 + (float)row * dx;
+        float z = -PI + dx / 2.0 + (float)slice * dx;
+
+        float f = 8 * cos(x) * cos(y) * cos(z) + cos(2 * x) * cos(2 * y) * cos(2 * z) - 
+                  (cos(2 * x) * cos(2 * y) + cos(2 * y) * cos(2 * z) + cos(2 * z) * cos(2 * x));
+
+        if (f > -b && f < b)
+        {
+            P[i] = 1;
+            count++;
+        }
+    }
+
+    info->VF = (float)count / (float)info->nElements;
+
+    return 0;
+}
+
 
 /*
     TPMS Function Lookup Table:
@@ -227,7 +615,10 @@ tpms_ptr TPMS_Functions[] = {
     SchwarzP,
     IWP,
     Neovius,
-    C_Y
+    C_Y,
+    Lidinoid,
+    OCTO,
+    FRD
 };
 
 #endif

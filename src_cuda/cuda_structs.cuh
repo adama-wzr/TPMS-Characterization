a0eb3c8f25fd30c71216197e3feafaf18f1fe00e
adama-wzr <adama@ku.edu>
2025-04-07 09:17:38 -0400

    Create cuda_structs.cuh
    - added data structure for simple multiGPU load balancing of the solver.

---
+++ src_cuda/cuda_structs.cuh
@@ -0,0 +1,23 @@
+
+#ifndef _CUDA_STRUC
+#define _CUDA_STRUC
+
+#include "cuda.h"
+#include "cuda_runtime.h"
+
+
+
+// GPU Context (multiGPU)
+
+typedef struct
+{
+    long int dataN;     // number of data points
+    // device buffers
+    float *d_Coeff, *d_Temp, *d_RHS, d_X;
+    // Stream for asynchronous execution
+    cudaStream_t stream;
+} MGPU_Solve;
+
+
+
+#endif

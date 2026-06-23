59dc3d75e069b996615f0fad57947bb3f90585b7
adama-wzr <andrefadam@gmail.com>
2026-06-23 17:05:39 -0400

    Create SF_Validation.cpp
    I just realized this is going to be a lot of work, specifically because I can't continue to use the same systems we had before based on iso-values (because it doesn't make sense for the theoretical cases)

---
+++ tests/SF_Validation.cpp
@@ -0,0 +1,26 @@
+/*
+ *
+ * This is a test function to validate the shape factor code 
+ * against some results that have analytic or theoretical solutions.
+ *
+ * Last modified: 06/23/2026
+ * Andre Adam.
+ */
+
+#include <stdio.h>
+#include <usrInput.hpp>
+#include <lib/sfSim.hpp>
+
+
+
+
+int main()
+{
+    // declare data structure with general user options
+    options opts;
+    meshInfo mesh;
+
+    // 
+
+    return 0;
+}
